7c087277bfbab3fc5ccda836007e7c5ec10c1592
03Jacobbattenfield <72627563+03Jacobbattenfield@users.noreply.github.com>
2020-10-25 18:28:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function answerright() {
+    document.getElementById("demo").innerHTML = "You Got That Hella Right!";
+}
+function answerwrong() {
+    document.getElementById("demo").innerHTML = "Sorry That Was Wrong Try Again";
+}
