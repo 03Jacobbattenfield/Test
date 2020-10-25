a375eb66192b665507bc691a7b45330809115dc9
03Jacobbattenfield <72627563+03Jacobbattenfield@users.noreply.github.com>
2020-10-25 18:28:15 -0500

    Delete fun.js

--- fun.js
+++
@@ -1,6 +0,0 @@
-function answerright() {
-    document.getElementById("demo").innerHTML = "You Got That Right, Good Job!";
-}
-function answerwrong() {
-    document.getElementById("demo").innerHTML = "Sorry That Was Wrong Try Again";
-}
