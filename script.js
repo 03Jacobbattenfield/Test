b0ce5a39296e2392380ae20014b964243866bd42
03Jacobbattenfield <72627563+03Jacobbattenfield@users.noreply.github.com>
2020-10-25 21:12:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+function answerright() {
+    document.getElementById("demo").innerHTML = "You Got That Hella Right!";
+    document.getElementById("correct?").innerHTML = "Correct!";
+        document.getElementById("correct?").style.color ="green";
+        document.getElementById("a1").style.backgroundColor="green";
+        document.getElementById("a2").style.backgroundColor="black";
+        document.getElementById("a3").style.backgroundColor="black";
+        document.getElementById("a4").style.backgroundColor="black";
+}
+function answerwrong() {
+    document.getElementById("demo").innerHTML = "Sorry That Was Wrong Try Again";
+    document.getElementById("correct?").innerHTML = "Wrong!";
+        document.getElementById("correct?").style.color ="Red";
+        document.getElementById("a2").style.backgroundColor="red";
+        document.getElementById("a1").style.backgroundColor="black";
+        document.getElementById("a3").style.backgroundColor="black";
+        document.getElementById("a4").style.backgroundColor="black";
+}
+function answerwrong2(){
+    document.getElementById("demo").innerHTML = "You Really Need To Brush Up On Your Arithmatic";
+    document.getElementById("correct?").innerHTML = "Wrong!";
+        document.getElementById("correct?").style.color ="Red";
+        document.getElementById("a3").style.backgroundColor="red";
+        document.getElementById("a1").style.backgroundColor="black";
+        document.getElementById("a2").style.backgroundColor="black";
+        document.getElementById("a4").style.backgroundColor="black";
+
+}
+function answerwrong3(){
+    document.getElementById("demo").innerHTML = "Try Not Sucking At Math";
+    document.getElementById("correct?").innerHTML = "Wrong!";
+        document.getElementById("correct?").style.color ="Red";
+        document.getElementById("a4").style.backgroundColor="red";
+        document.getElementById("a2").style.backgroundColor="black";
+        document.getElementById("a1").style.backgroundColor="black";
+        document.getElementById("a3").style.backgroundColor="black";
+}
+function answeright2(){
+    document.getElementById("demo2").innerHTML = "Why Do You Know This?";
+    document.getElementById("correct?2").innerHTML = "Correct!";
+        document.getElementById("correct?2").style.color="green";
+        document.getElementById("b1").style.backgroundColor="green";
+        document.getElementById("b2").style.backgroundColor="black";
+        document.getElementById("b3").style.backgroundColor="black";
+        document.getElementById("b4").style.backgroundColor="black";
+}
+function answerwrong4(){
+    document.getElementById("demo2").innerHTML = "Woah Dude Way Off";
+    document.getElementById("correct?2").innerHTML = "Wrong!";
+        document.getElementById("correct?2").style.color="red";
+        document.getElementById("b2").style.backgroundColor="red";
+        document.getElementById("b1").style.backgroundColor="black";
+        document.getElementById("b3").style.backgroundColor="black";
+        document.getElementById("b4").style.backgroundColor="black";
+}
+function answerwrong5(){
+    document.getElementById("demo2").innerHTML = "U Got It Wrong Stupid Dummy";
+    document.getElementById("correct?2").innerHTML = "Wrong!";
+        document.getElementById("correct?2").style.color="red";
+        document.getElementById("b3").style.backgroundColor="red";
+        document.getElementById("b1").style.backgroundColor="black";
+        document.getElementById("b2").style.backgroundColor="black";
+        document.getElementById("b4").style.backgroundColor="black";
+}
+function answerwrong6(){
+    document.getElementById("demo2").innerHTML = "You Done Messed Up Son";
+    document.getElementById("correct?2").innerHTML = "Wrong!";
+        document.getElementById("correct?2").style.color="red";
+        document.getElementById("b4").style.backgroundColor="red";
+        document.getElementById("b1").style.backgroundColor="black";
+        document.getElementById("b3").style.backgroundColor="black";
+        document.getElementById("b2").style.backgroundColor="black";
+}
